70f7f7ee0bb92899a4d5b1044c4e73d2273dfbad
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-07-21 13:11:55 +0530

    draw background

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 # Flappy Bird
 
-Using this FlappyBird project made by ImKennyYip to learn Java Swing 
-
+Using this FlappyBird project made by ImKennyYip to learn Java Swing how to create the game loop, create a jframe and jpanel, draw images on the jpanel, add click handlers to make the flappy bird jump, randomly generate pipes and move them across the screen, detect collisions between the flappy bird and each pipe, and add a running score.
 
 
 The `JAVA PROJECTS` view allows you to manage your dependencies. More details can be found [here](https://github.com/microsoft/vscode-java-dependency#manage-dependencies).
--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -4,25 +4,39 @@ import java.util.ArrayList;//storing all pipes in the game
 import java.util.Random;//placing pipes at random locations
 import javax.swing.*;
 
-public class FlappyBird extends JPanel{
-    int boardWidth=360;
-    int boardHeight=640;
+public class FlappyBird extends JPanel {
+    int boardWidth = 360;
+    int boardHeight = 640;
 
-    //Images
+    // Images
     Image backgroundImg;
     Image birdImg;
     Image topPipeImg;
     Image bottomPipeImg;
 
-    FlappyBird(){
-        setPreferredSize(new Dimension(boardWidth,boardHeight));
-        setBackground(Color.blue);
-    
-        //load images
-        backgroundImg=new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
-        birdImg=new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
-        topPipeImg=new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
-        bottomPipeImg=new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
-        //variable is of Image type but we assigned ImageIcon so we need to use getImage()
+    FlappyBird() {
+        setPreferredSize(new Dimension(boardWidth, boardHeight));
+        // setBackground(Color.blue);
+
+        // load images
+        backgroundImg = new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg = new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg = new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg = new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+        // variable is of Image type but we assigned ImageIcon so we need to use
+        // getImage()
     }
+
+    public void paintComponent(Graphics g) {
+        // since paintComponent is a part of Jpanel we can use super to call the super
+        // class implementation
+        super.paintComponent(g);
+        draw(g);
+    }
+
+    public void draw(Graphics g) {
+        // draws background'
+        g.drawImage(backgroundImg, 0, 0, boardWidth, boardHeight, null);
+    }
+
 }
