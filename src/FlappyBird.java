2a9edec3893870e8bc9e4d9f83ce74765ef4c922
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-07-21 12:32:58 +0530

    flappybird start

---
+++ src/FlappyBird.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.ArrayList;//storing all pipes in the game
+import java.util.Random;//placing pipes at random locations
+import javax.swing.*;
+
+public class FlappyBird extends JPanel{
+    
+}
