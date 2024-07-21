441f4bae0c23dd7d87fbd19b62240e0fdcb91847
cr7vaibhav <vaibhavkandwaloo7@gmail.com>
2024-07-21 12:53:57 +0530

    images

--- src/FlappyBird.java
+++ src/FlappyBird.java
@@ -8,9 +8,21 @@ public class FlappyBird extends JPanel{
     int boardWidth=360;
     int boardHeight=640;
 
+    //Images
+    Image backgroundImg;
+    Image birdImg;
+    Image topPipeImg;
+    Image bottomPipeImg;
+
     FlappyBird(){
         setPreferredSize(new Dimension(boardWidth,boardHeight));
         setBackground(Color.blue);
+    
+        //load images
+        backgroundImg=new ImageIcon(getClass().getResource("./flappybirdbg.png")).getImage();
+        birdImg=new ImageIcon(getClass().getResource("./flappybird.png")).getImage();
+        topPipeImg=new ImageIcon(getClass().getResource("./toppipe.png")).getImage();
+        bottomPipeImg=new ImageIcon(getClass().getResource("./bottompipe.png")).getImage();
+        //variable is of Image type but we assigned ImageIcon so we need to use getImage()
     }
-
 }
